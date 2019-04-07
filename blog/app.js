51cd2bea52687294d3fe366f72e99a52c8996b4d
Bogdana <bogdanalevickaa@gmail.com>
2019-04-07 15:08:18 +0300

    blog

---
+++ blog/app.js
@@ -0,0 +1,5 @@
+$('.slider').slick({
+	dots:true,
+	// autoplay:true,
+	// autoplaySpeed:2000,
+})
--- site/app.js
+++
@@ -1,8 +0,0 @@
-var productsCount = document.getElementById('products-count');
-var addToCartButtons = document.querySelectorAll(".add-to-card");
-for(var i = 0; i < addToCartButtons.length; i++){
-	addToCartButtons[i].addEventListener('click',function() {
-		var prevProductsCount = +productsCount.textContent;
-		productsCount.textContent = prevProductsCount + 1;
-	})
-}
